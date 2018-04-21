af86397e454d496f605ff7b50398e6c998bb262a
wjy50 <wjy50wjy@gmail.com>
2018-04-21 17:45:13 +0800

    增加softMax激活函数

---
+++ math/Activator.cpp
@@ -0,0 +1,33 @@
+/**
+ * Created by wjy50 on 18-4-21.
+ */
+
+#include "Activator.h"
+
+double sigmoid(double x) { return 1/(1+exp(-x)); }
+
+double dSigmoid_dx(double x)
+{
+    double sig = 1/(1+exp(-x)); /*sigmoid(x)*/
+    return sig*(1-sig);
+}
+
+double ReLU(double x) { return fmax(0, x); }
+
+double dReLU_dx(double x) { return x > 0 ? 1 : 0; }
+
+double lReLU(double x) { return x > 0 ? x : 0.1*x; }
+
+double dLReLU_dx(double x) { return x > 0 ? 1 : 0.1; }
+
+void softMax(double *x, int n)
+{
+    double m = 0;
+    for (int i = 0; i < n; ++i) {
+        x[i] = exp(x[i]);
+        m += x[i];
+    }
+    for (int i = 0; i < n; ++i) {
+        x[i] /= m;
+    }
+}
--- math/Activator.h
+++ math/Activator.h
@@ -5,28 +5,31 @@
 #ifndef NEURAL_NETWORK_ACTIVATOR_H
 #define NEURAL_NETWORK_ACTIVATOR_H
 
-#include <cmath>
+#include <complex>
 
 #ifdef __cplusplus
 extern "C"
 {
 #endif
 
-    double sigmoid(double x) { return 1/(1+std::exp(-x)); }
+    double sigmoid(double x);
 
-    double dSigmoid_dx(double x)
-    {
-        double sig = 1/(1+std::exp(-x)); /*sigmoid(x)*/
-        return sig*(1-sig);
-    }
+    double dSigmoid_dx(double x);
 
-    double ReLU(double x) { return std::fmax(0, x); }
+    double ReLU(double x);
 
-    double dReLU_dx(double x) { return x > 0 ? 1 : 0; }
+    double dReLU_dx(double x);
 
-    double lReLU(double x) { return x > 0 ? x : 0.1*x; }
+    double lReLU(double x);
 
-    double dLReLU_dx(double x) { return x > 0 ? 1 : 0.1; }
+    double dLReLU_dx(double x);
+
+    /**
+     * SoftMax激活函数，用于输出层
+     * @param x 输出层的带权输出向量
+     * @param n 维数
+     */
+    void softMax(double *x, int n);
 
 #ifdef __cplusplus
 };
