5693cbb94c858e4e54455821b822950fddc098d6
wjy50 <wjy50wjy@gmail.com>
2018-04-27 19:04:17 +0800

    randomPermutation模板化

--- math/permutation.h
+++ math/permutation.h
@@ -5,20 +5,29 @@
 #ifndef NEURAL_NETWORK_PERMUTATION_H
 #define NEURAL_NETWORK_PERMUTATION_H
 
-#ifdef __cplusplus
-extern "C"
-{
-#endif
+#include <random>
 
 /**
  * 生成从0到n的随机排列
  * @param arr 结果容器
  * @param n
  */
-void randomPermutation(int *arr, int n);
-
-#ifdef __cplusplus
-};
-#endif
+template<typename _IntType>
+void randomPermutation(_IntType *arr, _IntType n)
+{
+    static_assert(std::is_integral<_IntType>::value,
+                  "template argument not an integral type");
+    for (_IntType i = 0; i < n; ++i) {
+        arr[i] = i;
+    }
+    std::random_device rd;
+    std::uniform_int_distribution<_IntType> distribution(0, n);
+    for (_IntType i = n-1; i > 0; --i) {
+        _IntType d = distribution(rd) % i;
+        _IntType tmp = arr[d];
+        arr[d] = arr[i];
+        arr[i] = tmp;
+    }
+}
 
 #endif //NEURAL_NETWORK_PERMUTATION_H
