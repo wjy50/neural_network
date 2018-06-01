5b4328fd8ca0bbf985d27aa096563b5d9dfdcce1
wjy50 <wjy50wjy@gmail.com>
2018-06-01 10:34:54 +0800

    实现线性层的dropout

---
+++ v3/nn/layer/activation/DropoutLayer.cpp
@@ -0,0 +1,54 @@
+/**
+ * Created by wjy50 on 18-5-31.
+ */
+
+#include "DropoutLayer.h"
+#include "../../../utils/permutation.h"
+#include "../../../interface/interface.h"
+
+DropoutLayer::DropoutLayer(int dim) : LayerBase(dim, dim)
+{
+    dropoutCount = 0;
+    dropoutIds = new int[dim];
+}
+
+const FloatType* DropoutLayer::feedForward(const FloatType *x)
+{
+    return x;
+}
+
+const FloatType* DropoutLayer::feedForwardForOptimization(const FloatType *x)
+{
+    if (dropoutCount != 0) {
+        randomPermutation<int>(dropoutIds, outputDim);
+        linearDropout(const_cast<FloatType *>(x), outputDim, dropoutIds, dropoutCount, miniBatchSize);
+    }
+    return x;
+}
+
+void DropoutLayer::backPropagate(const FloatType *y)
+{
+    if (dropoutCount != 0) linearDropout(deltaOutput, outputDim, dropoutIds, dropoutCount, miniBatchSize);
+}
+
+void DropoutLayer::setDropoutFraction(FloatType f)
+{
+    dropoutCount = static_cast<int>(outputDim * f);
+}
+
+FloatType* DropoutLayer::getDelta()
+{
+    return deltaOutput;
+}
+
+bool DropoutLayer::needIndependentOutput()
+{
+    return false;
+}
+
+void DropoutLayer::computeGradients() {}
+
+DropoutLayer::~DropoutLayer()
+{
+    delete[] dropoutIds;
+}
---
+++ v3/nn/layer/activation/DropoutLayer.h
@@ -0,0 +1,39 @@
+/**
+ * Created by wjy50 on 18-5-31.
+ *
+ * DropoutLayer应当放在隐含LinearLayer的激活后面
+ */
+
+#ifndef NEURAL_NETWORK_DROPOUTLAYER_H
+#define NEURAL_NETWORK_DROPOUTLAYER_H
+
+
+#include "../base/LayerBase.h"
+
+class DropoutLayer : public LayerBase
+{
+private:
+    int dropoutCount;
+    int *dropoutIds;
+protected:
+    bool needIndependentOutput() override;
+
+    void computeGradients() override;
+public:
+    explicit DropoutLayer(int dim);
+
+    void setDropoutFraction(FloatType f);
+
+    const FloatType *feedForward(const FloatType *x) override;
+
+    const FloatType *feedForwardForOptimization(const FloatType *x) override;
+
+    void backPropagate(const FloatType *y) override;
+
+    FloatType *getDelta() override;
+
+    ~DropoutLayer() override;
+};
+
+
+#endif //NEURAL_NETWORK_DROPOUTLAYER_H
